cf2e9cfe1b70d71dcefb4fa0b044d91cb8dfd2a5
Nour <nourr313@proton.me>
2024-12-20 04:57:54 +0200

    -

---
+++ mst1584.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minCostConnectPoints = function (points) {
+    let n = points.length;
+    let visited = new Set();
+    let unvisited = new Set();
+    // Initialize unvisited set with values from 0 to n-1
+    for (let i = 0; i < n; i++) {
+        unvisited.add(i);
+    }
+    let cost = 0;
+    // priority queue that will store the point and cost
+    // and ranks them based on the cost
+    let pq = new MinPriorityQueue({ priority: (x) => x[1] });
+    pq.enqueue([0, 0]); // point, cost
+    while (!pq.isEmpty()) {
+        let [idx, weight] = pq.dequeue().element;
+        let point = points[idx];
+
+        if (visited.has(idx)) continue;
+        visited.add(idx);
+        unvisited.delete(idx);
+        cost += weight;
+        if (visited.size === n) break;
+
+        unvisited.forEach((i) => {
+            pq.enqueue([i, manhattanDistance(point, points[i])]);
+        });
+    }
+    return cost;
+};
+
+/**
+ * @param {number[]} a
+ * @param {number[]} b
+ * @return {number}
+ */
+function manhattanDistance(a, b) {
+    return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+}
